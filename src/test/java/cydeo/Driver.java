6cdcb5bf1f7d0c696655fa7c170175828d80226b
sjasot <sjasot@icloud.com>
2023-07-16 16:09:39 -0400

    updated jenkins file Driver config properties

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.234.84.104";
+                        String gridAddress = "54.242.48.250";   //54.234.84.104 it was here before
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
